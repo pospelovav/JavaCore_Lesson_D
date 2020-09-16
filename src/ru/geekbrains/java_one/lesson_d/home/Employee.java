69a315b7606c0bbc5e6a50d34699026788684807
pospelovav <shnurokvtaige@yandex.ru>
2020-09-16 23:05:39 +0300

    Создан метод EmployeeFourtyFiveSalaryUp, повышающий зарплату всем сотрудникам старше 45 лет на 5000. Добавлен метод avgSalaryAge, подсчитывающий средние арифметические зарплаты и возраста

--- src/ru/geekbrains/java_one/lesson_d/home/Employee.java
+++ src/ru/geekbrains/java_one/lesson_d/home/Employee.java
@@ -4,48 +4,47 @@ import java.util.Calendar;
 
 public class Employee {
     private String FullName;
-    private int Salary;
-    private int BirthYear;
+    private float Salary;
+    private float BirthYear;
+    private int Id;
+    private static int LastId = 0;  //хранит порядковый номер последнего добавленного сотрудника
 
-    public Employee(String FullName, int Salary, int Age){
+    Employee(String FullName, float Salary, float Age){
         setAge(Age);
         this.FullName = FullName;
         this.Salary = Salary;
+        this.Id = ++LastId;             //при создании объекта присваивает следующий Id
 
     }
 
-    private void setAge(int Age) {
+    private void setAge(float Age) {
         this.BirthYear = Calendar.getInstance().get(Calendar.YEAR) - Age;
     }
-    public int getAge(){
+    public float getAge(){
         return Calendar.getInstance().get(Calendar.YEAR) - BirthYear;
     }
-    /*
-    public void setFullName(String FullName){
-        this.FullName = FullName;
-    }
 
-     */
     public String getFullName() {
         return FullName;
     }
+    public int getId() {
+        return Id;
+    }
 
-    public int getSalary(){
+    public float getSalary(){
         return Salary;
     }
 
-    public void setSalary(int Salary){
+    public void setSalary(float Salary){
         this.Salary = Salary;
     }
 
     void OlderFourty(){
         if (getAge() > 40){
-            System.out.printf("FIO: %s, ZP: %s, age: %d\n",
-                    FullName, Salary, getAge());
+            System.out.printf("Id: %s, FIO: %s, ZP: %.02f, age: %.0f\n",
+                    Id, FullName, Salary, getAge());
         }
     }
-
-
 
 
 }
--- src/ru/geekbrains/java_one/lesson_d/home/Main.java
+++ src/ru/geekbrains/java_one/lesson_d/home/Main.java
@@ -1,15 +1,41 @@
 package ru.geekbrains.java_one.lesson_d.home;
 
 public class Main {
-    private static void EmployeeOlderFourty(Employee[] arr){
+    private static void EmployeeOlderFourty(Employee[] arr){    //вызывает метод печати информации о сотрудниках старше 40
         for (int i = 0; i < arr.length; i++) {
             arr[i].OlderFourty();
         }
     }
+    private static void EmployeeFourtyFiveSalaryUp(Employee[] arr){   //повышает зп на 5000 сотрудникам старше 45
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].getAge() > 45){
+                arr[i].setSalary(arr[i].getSalary() + 5000);
+            }
+        }
+    }
+    private static float avgSalaryAge(Employee[] arr, String avgWhat){
+        float AVG = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            switch (avgWhat){
+                case "Salary":
+                    AVG += arr[i].getSalary() / arr.length;
+                    break;
+                case "Age":
+                    AVG += arr[i].getAge() / arr.length;
+                    break;
+            }
+        }
+        return AVG;
+    }
+
+
     public static void main(String[] args){
         Employee Ivanov = new Employee("Ivanov Ivan", 10000, 32);
-        System.out.printf("FIO: %s, age: %d\n",
-                Ivanov.getFullName(), Ivanov.getAge());
+        System.out.printf("Id: %s, FIO: %s, age: %.0f\n",
+                Ivanov.getId(), Ivanov.getFullName(), Ivanov.getAge());
+        System.out.println();
 
         Employee[] arrEmloyees = new Employee[5];
         arrEmloyees[0] = new Employee("Petrov Vasya", 15000, 41);
@@ -19,7 +45,19 @@ public class Main {
         arrEmloyees[4] = new Employee("Kukushlin Stas", 22000, 38);
 
         EmployeeOlderFourty(arrEmloyees);
+        System.out.println();
 
+        EmployeeFourtyFiveSalaryUp(arrEmloyees);
+        for (int i = 0; i < arrEmloyees.length; i++) {
+            if (arrEmloyees[i].getAge() > 45){
+                System.out.printf("Id: %s, FIO: %s, ZP: %.02f, age: %.0f\n",
+                        arrEmloyees[i].getId(), arrEmloyees[i].getFullName(), arrEmloyees[i].getSalary(), arrEmloyees[i].getAge());
+            }
+        }
+        System.out.println();
+
+        System.out.printf("avgSalary: %.02f\n", avgSalaryAge(arrEmloyees, "Salary"));
+        System.out.printf("avgAge: %.02f\n", avgSalaryAge(arrEmloyees, "Age"));
 
     }
 
