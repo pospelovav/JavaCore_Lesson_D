a5a3a9f81aac0a968e7df49acf6b103d18239c77
pospelovav <shnurokvtaige@yandex.ru>
2020-09-16 21:55:34 +0300

    Создан массив из 5 сотрудников. Выведена информацию только о сотрудниках старше 40 лет;

--- src/ru/geekbrains/java_one/lesson_d/home/Employee.java
+++ src/ru/geekbrains/java_one/lesson_d/home/Employee.java
@@ -17,14 +17,35 @@ public class Employee {
     private void setAge(int Age) {
         this.BirthYear = Calendar.getInstance().get(Calendar.YEAR) - Age;
     }
-    public String getFullName() {
-        return FullName;
-    }
-    public int getSalary(){
-        return Salary;
-    }
     public int getAge(){
         return Calendar.getInstance().get(Calendar.YEAR) - BirthYear;
     }
+    /*
+    public void setFullName(String FullName){
+        this.FullName = FullName;
+    }
+
+     */
+    public String getFullName() {
+        return FullName;
+    }
+
+    public int getSalary(){
+        return Salary;
+    }
+
+    public void setSalary(int Salary){
+        this.Salary = Salary;
+    }
+
+    void OlderFourty(){
+        if (getAge() > 40){
+            System.out.printf("FIO: %s, ZP: %s, age: %d\n",
+                    FullName, Salary, getAge());
+        }
+    }
+
+
+
 
 }
--- src/ru/geekbrains/java_one/lesson_d/home/Main.java
+++ src/ru/geekbrains/java_one/lesson_d/home/Main.java
@@ -1,10 +1,25 @@
 package ru.geekbrains.java_one.lesson_d.home;
 
 public class Main {
+    private static void EmployeeOlderFourty(Employee[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            arr[i].OlderFourty();
+        }
+    }
     public static void main(String[] args){
         Employee Ivanov = new Employee("Ivanov Ivan", 10000, 32);
         System.out.printf("FIO: %s, age: %d\n",
                 Ivanov.getFullName(), Ivanov.getAge());
+
+        Employee[] arrEmloyees = new Employee[5];
+        arrEmloyees[0] = new Employee("Petrov Vasya", 15000, 41);
+        arrEmloyees[1] = new Employee("Sidorov Petr", 25000, 30);
+        arrEmloyees[2] = new Employee("Ivanova Vika", 30000, 46);
+        arrEmloyees[3] = new Employee("Petrova Tanya", 18000, 36);
+        arrEmloyees[4] = new Employee("Kukushlin Stas", 22000, 38);
+
+        EmployeeOlderFourty(arrEmloyees);
+
 
     }
 
