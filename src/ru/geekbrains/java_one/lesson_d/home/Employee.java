d5890cdeeb28612f728f35ad3090ffdd60e40302
pospelovav <shnurokvtaige@yandex.ru>
2020-09-16 20:44:14 +0300

    Добавлен конструктор класса, заполняющий поля при создании объекта; Созданы, которые возвращают значение каждого поля;

--- src/ru/geekbrains/java_one/lesson_d/home/Employee.java
+++ src/ru/geekbrains/java_one/lesson_d/home/Employee.java
@@ -1,8 +1,30 @@
 package ru.geekbrains.java_one.lesson_d.home;
 
+import java.util.Calendar;
+
 public class Employee {
     private String FullName;
     private int Salary;
-    private int Age;
+    private int BirthYear;
+
+    public Employee(String FullName, int Salary, int Age){
+        setAge(Age);
+        this.FullName = FullName;
+        this.Salary = Salary;
+
+    }
+
+    private void setAge(int Age) {
+        this.BirthYear = Calendar.getInstance().get(Calendar.YEAR) - Age;
+    }
+    public String getFullName() {
+        return FullName;
+    }
+    public int getSalary(){
+        return Salary;
+    }
+    public int getAge(){
+        return Calendar.getInstance().get(Calendar.YEAR) - BirthYear;
+    }
 
 }
--- src/ru/geekbrains/java_one/lesson_d/home/Main.java
+++ src/ru/geekbrains/java_one/lesson_d/home/Main.java
@@ -1,4 +1,12 @@
 package ru.geekbrains.java_one.lesson_d.home;
 
 public class Main {
+    public static void main(String[] args){
+        Employee Ivanov = new Employee("Ivanov Ivan", 10000, 32);
+        System.out.printf("FIO: %s, age: %d\n",
+                Ivanov.getFullName(), Ivanov.getAge());
+
+    }
+
+
 }
