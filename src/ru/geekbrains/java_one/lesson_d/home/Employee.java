9af9d70167e9fb7d464415391b679c7f86468ae3
pospelovav <shnurokvtaige@yandex.ru>
2020-09-16 20:07:26 +0300

    class Main class Employee

---
+++ src/ru/geekbrains/java_one/lesson_d/home/Employee.java
@@ -0,0 +1,8 @@
+package ru.geekbrains.java_one.lesson_d.home;
+
+public class Employee {
+    private String FullName;
+    private int Salary;
+    private int Age;
+
+}
---
+++ src/ru/geekbrains/java_one/lesson_d/home/Main.java
@@ -0,0 +1,4 @@
+package ru.geekbrains.java_one.lesson_d.home;
+
+public class Main {
+}
